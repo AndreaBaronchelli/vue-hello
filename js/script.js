55de6188f1a69d49d74b5f3f5ec7b6560b4eb040
AndreaBaronchelli <baronchelli.andrea@gmail.com>
2021-04-20 14:24:45 +0200

    life on mars

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        title: "Life on Mars",
+        image: "https://www.nasa.gov/sites/default/files/thumbnails/image/pia24498.jpeg"
+    }
+});
